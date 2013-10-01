41db43fe50261363e051b3adb26165de1e0242b3
Ben Casey <bcasey@mines.edu>
2013-10-01 00:53:44 -0600

    added input dialog, and long delete

--- SchoolSchedulerCaseySoto/src/edu/mines/caseysoto/schoolschedulercaseysoto/MainActivity.java
+++ SchoolSchedulerCaseySoto/src/edu/mines/caseysoto/schoolschedulercaseysoto/MainActivity.java
@@ -10,18 +10,22 @@ import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
+import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 
 @SuppressLint("NewApi")
-public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>{
+public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>, InputDialogFragment.Listener{
 
 	private SimpleCursorAdapter adapter;
 	private static final int DELETE_ID = Menu.FIRST + 1;
+	private String courseName;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -42,21 +46,35 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
 		return true;
 	}
 	public void onDialog(View view){
-		
+		Bundle args = new Bundle();
+        args.putInt( "dialogID", 1 );
+        args.putString( "prompt", getString( R.string.statement ) );
+
+        InputDialogFragment dialog = new InputDialogFragment();
+        dialog.setArguments( args );
+        dialog.show( getFragmentManager(), "Dialog" );
 		
 	}
 	public void check(){
 		ContentValues values = new ContentValues();
-		//values.put(CourseTable.COLUMN_ID, "idd");
 	    values.put( CourseTable.COLUMN_NAME, "CSCI498" );
-	    String[] projection = { CourseTable.COLUMN_ID, CourseTable.COLUMN_NAME};
-	    //Uri uri = 
+	    String[] projection = { CourseTable.COLUMN_ID, CourseTable.COLUMN_NAME}; 
 	    Cursor cursor = getContentResolver().query( CourseContentProvider.CONTENT_URI, projection, null, null, null );
 	    if(cursor.getCount() <= 0){
 	    	Uri CourseUri = getContentResolver().insert( CourseContentProvider.CONTENT_URI, values );
 	    }
+	    cursor.close();
 	}
-	
+	public void insertNewCourse(){
+		ContentValues values = new ContentValues();
+		//values.put(CourseTable.COLUMN_ID, "idd");
+	    values.put( CourseTable.COLUMN_NAME, courseName );
+	    String[] projection = { CourseTable.COLUMN_ID, CourseTable.COLUMN_NAME};
+	    //TODO: need to figure out how not to enter in same course
+	    Cursor cursor = getContentResolver().query( CourseContentProvider.CONTENT_URI, projection, null, null, null );
+	    Uri CourseUri = getContentResolver().insert( CourseContentProvider.CONTENT_URI, values );
+	    cursor.close();
+	}
 	@Override
 	  protected void onListItemClick( ListView l, View v, int position, long id )
 	  {
@@ -110,8 +128,6 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
 	    // Ensure a loader is initialized and active.
 	    getLoaderManager().initLoader( 0, null, this );
 	    
-	    
-	    
 	    // Note the last parameter to this constructor (zero), which indicates the adaptor should
 	    // not try to automatically re-query the data ... the loader will take care of this.
 	    this.adapter = new SimpleCursorAdapter( this, R.layout.list_row, null, from, to, 0 );
@@ -121,4 +137,36 @@ public class MainActivity extends ListActivity implements LoaderManager.LoaderCa
 	    setListAdapter( this.adapter );
 	  }
 	
+	@Override
+	  public void onInputDone( int dialogID, String input )
+	  {
+	    Log.d( "School_Scheduler", "\"" + input + "\" received from input dialog with id =" + dialogID );
+	    
+	    if(dialogID == 1){
+	      this.courseName = input;
+	      insertNewCourse();
+	    }
+	     
+	  }
+
+	  /**
+	   * Callback method from InputDialogFragment when the user clicks Cancel.
+	   * 
+	   * @param dialogID The dialog producing the callback.
+	   */
+	  @Override
+	  public void onInputCancel( int dialogID )
+	  {
+	    Log.d( "School_Scheduler", "No input received from input dialog with id =" + dialogID );
+	  }
+	  
+	  /** The menu displayed on a long touch. */
+	  @Override
+	  public void onCreateContextMenu( ContextMenu menu, View v, ContextMenuInfo menuInfo )
+	  {
+	    super.onCreateContextMenu( menu, v, menuInfo );
+	    menu.add( 0, DELETE_ID, 0, R.string.menu_delete );
+	  }
+
+	  
 }
