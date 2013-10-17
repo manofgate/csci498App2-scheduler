68737f2c5b778178509270660efe111956e64695
Craig Soto II <csoto@mines.edu>
2013-10-16 22:39:44 -0600

    documentation for AddHomeworkActivity

--- SchoolSchedulerCaseySoto/src/edu/mines/caseysoto/schoolschedulercaseysoto/AddHomeworkActivity.java
+++ SchoolSchedulerCaseySoto/src/edu/mines/caseysoto/schoolschedulercaseysoto/AddHomeworkActivity.java
@@ -1,4 +1,24 @@
-//http://stackoverflow.com/questions/7200108/android-gettext-from-edittext-field
+/**
+ * Description: This class takes in user input for the homework's name, due date, and description. 
+ * 	Once the user submits this data it validates and normalizes the input and adds it to the database. 
+ * 	The class handles adding new homework as well as updating homework.  
+ *
+ * @author Craig J. Soto II
+ * @author Ben Casey
+ */
+
+
+/*
+ * Web pages that helped contribute to writing the code for this class:
+ * 
+ * http://stackoverflow.com/questions/7200108/android-gettext-from-edittext-field
+ * http://stackoverflow.com/questions/6021836/getting-listitem-data-from-a-list
+ * http://stackoverflow.com/questions/18480633/java-util-date-format-conversion-yyyy-mm-dd-to-mm-dd-yyyy
+ * http://stackoverflow.com/questions/9629636/get-todays-date-in-java
+ * http://thinkandroid.wordpress.com/2010/01/09/simplecursoradapters-and-listviews/
+ * http://stackoverflow.com/questions/18967790/listview-that-shows-multiple-columns-of-one-row-instead-of-multiple-rows
+ * http://kahdev.wordpress.com/2010/09/27/android-using-the-sqlite-database-with-listview/
+ */
 
 package edu.mines.caseysoto.schoolschedulercaseysoto;
 
@@ -16,37 +36,46 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.Cursor;
 
 public class AddHomeworkActivity extends Activity {
-	private final static int DESC_MAX = 140;
-	private View mCourseText;
-	private View hwNameText;
-	private View dateText;
-	private View descText;
-	private boolean update = false;
-	private String hwName = "";
-	private String date = "";
-	private String description = "";
 
+	private final static int DESC_MAX = 140; //used to limit the users description to 140 characters
+	private boolean update = false; //used to help determine if homework is being updated or not
+	private String hwName = ""; //used for checking if name needs to be updated
+	private String date = ""; //used for checking if date needs to be updated
+	private String description = ""; //used for checking if description needs to be updated
+
+	/**
+	 * The onCreate method retrieves any saved instances and sets the content view layout. It
+	 * retrieves and sets the course name, homwork name, due date, and description (if present
+	 * from the HomeworkActivity, fills the respective TextViews.    
+	 * 
+	 * @param savedInstanceState - a bundle of any saved instance values 
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_homework);
 
-		mCourseText = findViewById(R.id.courseNameEnd);
-		hwNameText = findViewById(R.id.nameInput);
-		dateText = findViewById(R.id.dateInput);
-		descText = findViewById(R.id.descriptionInput);
+		//Retrieve strings passed in from the HomeworkActivity
+		String message = getIntent().getStringExtra( MainActivity.COURSE_MNAME);
+		hwName = getIntent().getStringExtra( MainActivity.HW_NAME_TEXT);
+		date = getIntent().getStringExtra( MainActivity.DATE_TEXT);
+		description = getIntent().getStringExtra( MainActivity.DESC_TEXT);
 
-		//get the message from the intent
-		Intent intent = getIntent();
-		String message = intent.getStringExtra( MainActivity.COURSE_MNAME);
-		hwName = intent.getStringExtra( MainActivity.HW_NAME_TEXT);
-		date = intent.getStringExtra( MainActivity.DATE_TEXT);
-		description = intent.getStringExtra( MainActivity.DESC_TEXT);
-		((TextView) mCourseText).setText(message);
+		//Get the TextView item to be updated
+		TextView mCourseText = (TextView) findViewById(R.id.courseNameEnd);
+		TextView hwNameText = (TextView) findViewById(R.id.nameInput);
+		TextView dateText = (TextView) findViewById(R.id.dateInput);
+		TextView descText = (TextView) findViewById(R.id.descriptionInput);
+
+		//Set the TextView item to the new text form the HomeworkActivity
+		mCourseText.setText(message);
+
+		//If HomeworkActivity will be updating info, the hwName won't be empty nor will description
+		//or date guaranteed (we normalize the info put into the db so we will always have this). We
+		//also set the update value to true to follow update logic. 
 		if(!hwName.equals("")){
 			((TextView) hwNameText).setText(hwName);
 			((TextView) descText).setText(description);
@@ -55,7 +84,15 @@ public class AddHomeworkActivity extends Activity {
 		}
 	}
 
+	/**
+	 * The submit method retrieves the EditText content for the name, due date, and description from
+	 * the activity. It also validates and normalizes the user input, updates or inserts the input,
+	 * then finishes the activity.   
+	 * 
+	 * @param view - this is necessary for the button to interact with the activity
+	 */
 	public void submit(View view){
+		//Retrieve the user input
 		EditText nameInput = (EditText) findViewById(R.id.nameInput);
 		EditText descriptionInput = (EditText) findViewById(R.id.descriptionInput);
 		EditText dateInput = (EditText) findViewById(R.id.dateInput);
@@ -63,33 +100,35 @@ public class AddHomeworkActivity extends Activity {
 		String hwName = nameInput.getText().toString();
 		String desc = descriptionInput.getText().toString();
 		String dueDate = dateInput.getText().toString();
-		Intent intent = getIntent();
-		String course = intent.getStringExtra( MainActivity.COURSE_MNAME);
+		String course = getIntent().getStringExtra( MainActivity.COURSE_MNAME);
 
-		if(hwName.length() > 0 && desc.length() > 0){
-			if(desc.length() > DESC_MAX) {
-				desc = desc.substring(0, DESC_MAX);
-			}
-		} else {
-			hwName = "Untitled";
-			desc = "None";
+		//Make sure the name and desc have content, if not give it generic information. 
+		hwName = hwName.length() > 0 ? hwName : "Untitled";
+		desc = desc.length() > 0 ? desc : "None";
+
+		//Trim the desc to 140 characters
+		if(desc.length() > DESC_MAX) {
+			desc = desc.substring(0, DESC_MAX);
 		}
 
-		if(checkDate(dueDate) || update){
-			Toast toast = Toast.makeText(getApplicationContext(), "Name: " + hwName + " Desc: " + desc + " Due Date: " + dueDate, Toast.LENGTH_LONG);
-			toast.show();
-
+		//Validate that the date is in the correct format and in the future. 
+		if(checkDate(dueDate)){
+			//Normalize dueDate
 			if(!dueDate.contains("-")){
 				dueDate = dueDate.substring(0, 2) + "-" + dueDate.substring(2, 4) + "-" + dueDate.substring(4, 8);
 			}
 		} else {
+			//Notify the user that the date was incorrect. 
 			Toast toast = Toast.makeText(getApplicationContext(), "Incorrect date format. Set to today's date." , Toast.LENGTH_LONG);
 			toast.show();
-			Calendar currentDate = Calendar.getInstance(); //Get the current date
-			SimpleDateFormat today = new SimpleDateFormat("MM-dd-yyyy", java.util.Locale.getDefault()); //format it as per your requirement
+
+			//Set dueDate to today's date. 
+			Calendar currentDate = Calendar.getInstance(); 
+			SimpleDateFormat today = new SimpleDateFormat("MM-dd-yyyy", java.util.Locale.getDefault()); 
 			dueDate = today.format(currentDate.getTime());
 		}
 
+		//Call the respective method based on what the user is doing
 		if(update){
 			updateHW(hwName, dueDate, desc);
 		} else {
@@ -99,8 +138,60 @@ public class AddHomeworkActivity extends Activity {
 		finish();
 	}
 
+	/**
+	 * The checkDate method checks that the input is a date, the date is in mmddyyyy format, and that
+	 * it is in the future. If it doesn't meet this criteria, it will return false.  
+	 * 
+	 * @param dateInput - the user input date
+	 * 
+	 * @return correctInput - return if input is valid
+	 */
+	private boolean checkDate(String dateInput) {
+		boolean correctInput = false;
+
+		//Add hyphens to the dateInput for verifying
+		if(dateInput.length() == 8){
+			dateInput = dateInput.substring(0, 2) + "-" + dateInput.substring(2, 4) + "-" + dateInput.substring(4, 8);
+		}
+
+		if(dateInput.length() == 10){
+			//Format that will be usedfor the date
+			SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy", java.util.Locale.getDefault());
+
+			//Get current date and convert it to a string
+			Calendar currentDate = Calendar.getInstance();
+			String dateNow = sdf.format(currentDate.getTime());
+
+			//Parse user input date and today's date to Date objects then make sure user date not
+			//before (can be today's date or future date) today's date. If any input is incorrect, 
+			//catch the exception and don't set correctInput to true;
+			try {
+				Date dueDate = sdf.parse(dateInput);
+				Date today = sdf.parse(dateNow);
+				correctInput = !dueDate.before(today);
+			} catch (ParseException e) {
+				correctInput = false;
+				Log.d("ADDHOMEWORKACTIVITY", "Error parsing dates." + e);
+			}
+		}
+
+		return correctInput;
+	}
+	
+	/**
+	 * The updateHW method checks to see if the name, due date, or description needs to be updated. 
+	 * If any of them need to be updated then update it. 
+	 * 
+	 * @param name - name retrieved from Activity
+	 * @param dueDate - date retrieved from Activity
+	 * @param desc - description retrieved from Activity
+	 */
 	private void updateHW(String name, String dueDate, String desc) {
 		int rowsUpdated = 0;
+		
+		//If the name/date/description was updated by the user it won't match the values that were passed
+		//from HomeworkActivity when the user clicked a homework to be updated. In this case, updated
+		//that item respectively. 
 		ContentValues values = new ContentValues();
 		if(!name.equals(hwName)){
 			values.put( HomeworkTable.COLUMN_NAME, name );
@@ -121,53 +212,30 @@ public class AddHomeworkActivity extends Activity {
 		if(rowsUpdated == 0){
 			Log.d("ADDHOMEWORK", "Now rows were updated");
 		}
-		finish();
 	}
 
-	private boolean checkDate(String dateInput) {
-		boolean correctInput = false;
-
-		if(dateInput.length() == 8){
-			dateInput = dateInput.substring(0, 2) + "-" + dateInput.substring(2, 4) + "-" + dateInput.substring(4, 8);
-		}
-
-		if(dateInput.length() == 10){
-			SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy", java.util.Locale.getDefault());
-
-			Calendar currentDate = Calendar.getInstance(); //Get the current date
-			SimpleDateFormat today= new SimpleDateFormat("MM-dd-yyyy", java.util.Locale.getDefault()); //format it as per your requirement
-			String dateNow = today.format(currentDate.getTime());
-
-			try {
-				Date date1 = sdf.parse(dateInput);
-				Date date2 = today.parse(dateNow);
-				correctInput = !date1.before(date2);
-			} catch (ParseException e) {
-				correctInput = false;
-				Log.d("ADDHOMEWORKACTIVITY", "Error parsing dates." + e);
-			}
-		} else {
-			Toast toast = Toast.makeText(getApplicationContext(), "Incorrect date format. Set to today's date." , Toast.LENGTH_LONG);
-			toast.show();
-		}
-
-		return correctInput;
-	}
-
+	/**
+	 * The insertNewHW method checks to see if the name, due date, or description needs to be updated. 
+	 * If any of them need to be updated then update it.   
+	 * 
+	 * @param view - this is necessary for the button to interact with the activity
+	 */
 	public void insertNewHW(String name, String date, String desc, String course){
 		ContentValues values = new ContentValues();
 		values.put( HomeworkTable.COLUMN_NAME, name );
 		values.put( HomeworkTable.COLUMN_DATE, date );
 		values.put( HomeworkTable.COLUMN_DESCRIPTION, desc);
 		values.put( HomeworkTable.COLUMN_COURSE_NAME, course);
-
-		String[] projection = { HomeworkTable.COLUMN_ID, HomeworkTable.COLUMN_NAME};
-		String[] selection = {name};
-
+		
+		//Insert values into the Homework Table
 		getContentResolver().insert( SchedulerContentProvider.CONTENT_URI_H, values );
 
-		//checks to see if that course name has already been added
+		//Verify if identical entries were inserted into the Homework Table 
+		String[] projection = { HomeworkTable.COLUMN_ID, HomeworkTable.COLUMN_NAME};
+		String[] selection = {name};
 		Cursor cursor = getContentResolver().query( SchedulerContentProvider.CONTENT_URI_H, projection, "name=?", selection, HomeworkTable.COLUMN_ID + " DESC" );
+		
+		//If there were multiple entries remove the last insert then notify the user. 
 		if(cursor.getCount() > 1){
 			cursor.moveToFirst();
 			Uri courseUri = Uri.parse( SchedulerContentProvider.CONTENT_URI_H + "/" +  cursor.getString(cursor.getColumnIndexOrThrow( HomeworkTable.COLUMN_ID )) );
--- SchoolSchedulerCaseySoto/src/edu/mines/caseysoto/schoolschedulercaseysoto/HomeworkActivity.java
+++ SchoolSchedulerCaseySoto/src/edu/mines/caseysoto/schoolschedulercaseysoto/HomeworkActivity.java
@@ -1,11 +1,11 @@
 /**
-* Description: This class displays the list of homework based on which course was selected. The 
-* 	user will be able to either add a homework (touching the add button), edit a homework (touching
-* 	the homework in the list), and deleting a homework (long press homework in list). 
-*
-* @author Craig J. Soto II
-* @author Ben Casey
-*/
+ * Description: This class displays the list of homework based on which course was selected. The 
+ * 	user will be able to either add a homework (touching the add button), edit a homework (touching
+ * 	the homework in the list), and deleting a homework (long press homework in list). 
+ *
+ * @author Craig J. Soto II
+ * @author Ben Casey
+ */
 
 /*
  * Web pages that helped contribute to writing the code for this class:
@@ -45,75 +45,75 @@ public class HomeworkActivity extends ListActivity implements LoaderManager.Load
 	private String courseName; //receives and passes on course name from the MainActivity
 
 	/**
-	* The onCreate method retrieves any saved instances and sets the content view layout. It
-	* retrieves and sets the course name from the MainActivity, fills the homework table with the 
-	* respective course's homework, and sets up the context menu.   
-	* 
-	* @param savedInstanceState - a bundle of any saved instance values 
-	*/
+	 * The onCreate method retrieves any saved instances and sets the content view layout. It
+	 * retrieves and sets the course name from the MainActivity, fills the homework table with the 
+	 * respective course's homework, and sets up the context menu.   
+	 * 
+	 * @param savedInstanceState - a bundle of any saved instance values 
+	 */
 	@Override
 	public void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.homework_list );
-		
+
 		//Get/set the courseName in the activity
 		courseName = getIntent().getStringExtra( MainActivity.COURSE_MNAME);
 		TextView mCourseText = (TextView)findViewById(R.id.courseNameMid);
 		mCourseText.setText(courseName);
-		
+
 		//Set up ListView
 		this.getListView().setDividerHeight( 2 );
 		registerForContextMenu( getListView() );
-		
+
 		//Fill the Listview table
 		fillData();
 	}
 
 	/**
-	* The onCreateLoader loads the homework specific to the course. This makes sure we only see the 
-	* homework for that course and no others. 
-	* 
-	* @param arg0 - unused but needed for abstract method
-	* @param arg1 - unused but needed for abstract method
-	* 
-	* @return cursorLoader - the homework information from the database
-	*/
+	 * The onCreateLoader loads the homework specific to the course. This makes sure we only see the 
+	 * homework for that course and no others. 
+	 * 
+	 * @param arg0 - unused but needed for abstract method
+	 * @param arg1 - unused but needed for abstract method
+	 * 
+	 * @return cursorLoader - the homework information from the database
+	 */
 	@Override
 	public Loader<Cursor> onCreateLoader(int arg0, Bundle arg1) {
 		Log.d("SchoolScheduler::OnlyCourse", "This Course name is "+ courseName);
-		
+
 		//Retrieve homework info from database
 		String[] projection = { HomeworkTable.COLUMN_ID, HomeworkTable.COLUMN_NAME, HomeworkTable.COLUMN_DATE, HomeworkTable.COLUMN_DESCRIPTION, HomeworkTable.COLUMN_COURSE_NAME };
 		String[] selection = {courseName};
 		CursorLoader cursorLoader = new CursorLoader( this, SchedulerContentProvider.CONTENT_URI_H, projection, "course=?", selection, null );
-		
+
 		return cursorLoader;
 	}
 
 	/**
-	* The onLoadFinished is a needed abstract method to help load the data in the ListView
-	* 
-	* @param arg0 - unused but needed for abstract method
-	* @param arg1 - used when swaping cursor
-	*/
+	 * The onLoadFinished is a needed abstract method to help load the data in the ListView
+	 * 
+	 * @param arg0 - unused but needed for abstract method
+	 * @param arg1 - used when swaping cursor
+	 */
 	@Override
 	public void onLoadFinished(Loader<Cursor> arg0, Cursor arg1) {
 		this.adapter.swapCursor( arg1 );
 	}
-	
+
 	/**
-	* The onLoadReset is a needed abstract method to help load the data in the ListView
-	* 
-	* @param arg0 - unused but needed for abstract method
-	*/
+	 * The onLoadReset is a needed abstract method to help load the data in the ListView
+	 * 
+	 * @param arg0 - unused but needed for abstract method
+	 */
 	@Override
 	public void onLoaderReset(Loader<Cursor> arg0) {
 		this.adapter.swapCursor( null );
 	}
 
 	/**
-	* The fillData method fills the ListView with the homework. 
-	*/
+	 * The fillData method fills the ListView with the homework. 
+	 */
 	private void fillData() {
 		//Fields in the DB from which we map 
 		String[] from = new String[] { HomeworkTable.COLUMN_NAME, HomeworkTable.COLUMN_DATE, HomeworkTable.COLUMN_DESCRIPTION };
@@ -129,30 +129,30 @@ public class HomeworkActivity extends ListActivity implements LoaderManager.Load
 		this.adapter = new SimpleCursorAdapter( this, R.layout.home_list_row, null, from, to, 0 ){
 			//Change the color of each ListItem to help the user
 			@Override
-		    public View getView(int position, View convertView, ViewGroup parent) {
-				 View v = super.getView(position, convertView, parent);
+			public View getView(int position, View convertView, ViewGroup parent) {
+				View v = super.getView(position, convertView, parent);
 
-			        if (position %2 ==1) {
-			            v.setBackgroundColor(Color.argb(TRIM_MEMORY_MODERATE, 100, 100, 100));
-			        } else {
-			            v.setBackgroundColor(Color.argb(TRIM_MEMORY_MODERATE, 170, 170, 170)); //or whatever was original
-			        }
+				if (position %2 ==1) {
+					v.setBackgroundColor(Color.argb(TRIM_MEMORY_MODERATE, 100, 100, 100));
+				} else {
+					v.setBackgroundColor(Color.argb(TRIM_MEMORY_MODERATE, 170, 170, 170)); //or whatever was original
+				}
 
-			        return v;
+				return v;
 			}
-			
+
 		};
-		
+
 		// Let this ListActivity display the contents of the cursor adapter.
 		setListAdapter( this.adapter );
 	}
 
 	/**
-	* The addHomeworkToList method starts the AddHomeworkActivity. It also sets the needed elements
-	* used in that activity. 
-	* 
-	* @param view - this is necessary for the button to interact with the activity
-	*/
+	 * The addHomeworkToList method starts the AddHomeworkActivity. It also sets the needed elements
+	 * used in that activity. 
+	 * 
+	 * @param view - this is necessary for the button to interact with the activity
+	 */
 	public void addHomeworkToList(View view) {
 		Intent intent = new Intent(this, AddHomeworkActivity.class);
 		intent.putExtra(MainActivity.COURSE_MNAME, courseName);
@@ -163,28 +163,28 @@ public class HomeworkActivity extends ListActivity implements LoaderManager.Load
 		intent.putExtra(MainActivity.DESC_TEXT, "");
 		startActivity(intent);
 	}
-	
+
 	/**
-	* The onCreateContextMenu method sets up the menu displayed on a long touch.
-	* 
-	* @param menu - the menu that is created to hold menu options
-	* @param v - view that will interact with the UI
-	* @param menuInfo - information needed for the menu
-	*/
+	 * The onCreateContextMenu method sets up the menu displayed on a long touch.
+	 * 
+	 * @param menu - the menu that is created to hold menu options
+	 * @param v - view that will interact with the UI
+	 * @param menuInfo - information needed for the menu
+	 */
 	@Override
 	public void onCreateContextMenu( ContextMenu menu, View v, ContextMenuInfo menuInfo ) {
 		super.onCreateContextMenu( menu, v, menuInfo );
 		menu.add( 0, DELETE_ID, 0, R.string.menu_delete );
 	}
-	
+
 	/**
-	* The onCreateContextItemSelected method handles the logic when an item in the context menu
-	* is selected. 
-	* 
-	* @param item - item that was selected
-	* 
-	* @return super.onContextItemSelected(item) - returns the item that was deleted. 
-	*/
+	 * The onCreateContextItemSelected method handles the logic when an item in the context menu
+	 * is selected. 
+	 * 
+	 * @param item - item that was selected
+	 * 
+	 * @return super.onContextItemSelected(item) - returns the item that was deleted. 
+	 */
 	@Override
 	public boolean onContextItemSelected( MenuItem item ) {
 		switch( item.getItemId() ) {
@@ -198,29 +198,29 @@ public class HomeworkActivity extends ListActivity implements LoaderManager.Load
 		}
 		return super.onContextItemSelected( item );
 	}
-	
+
 	/**
-	* The onListItemClick method retrieves the information from the list, queries the database, 
-	* sets the respective variables with that info, then starts the AddHomeworkActivity for editing
-	* purposes. 
-	* 
-	* @param l - the list from the Activity
-	* @param v - the view from the Activity
-	* @param position - the list item position in the list
-	* @param id - the id of the list item
-	*/
+	 * The onListItemClick method retrieves the information from the list, queries the database, 
+	 * sets the respective variables with that info, then starts the AddHomeworkActivity for editing
+	 * purposes. 
+	 * 
+	 * @param l - the list from the Activity
+	 * @param v - the view from the Activity
+	 * @param position - the list item position in the list
+	 * @param id - the id of the list item
+	 */
 	@Override
 	protected void onListItemClick( ListView l, View v, int position, long id ) {
 		super.onListItemClick( l, v, position, id );
-		
+
 		//Get the AddHomeworkActivity intent
 		Intent i = new Intent( this, AddHomeworkActivity.class );
-		
+
 		//Query the database for the necessary information
 		Uri courseUri = Uri.parse( SchedulerContentProvider.CONTENT_URI_H + "/" + id );
 		String[] projection = { HomeworkTable.COLUMN_NAME, HomeworkTable.COLUMN_DATE, HomeworkTable.COLUMN_DESCRIPTION, HomeworkTable.COLUMN_COURSE_NAME };
 		Cursor cursor = getContentResolver().query( courseUri, projection, null, null, null );
-		
+
 		//Retrieve the information from the database. 
 		cursor.moveToFirst();	    
 		String name = cursor.getString( cursor.getColumnIndexOrThrow( HomeworkTable.COLUMN_NAME ) );
@@ -228,13 +228,13 @@ public class HomeworkActivity extends ListActivity implements LoaderManager.Load
 		String desc = cursor.getString( cursor.getColumnIndexOrThrow( HomeworkTable.COLUMN_DESCRIPTION ) );
 		String courseName = cursor.getString( cursor.getColumnIndexOrThrow( HomeworkTable.COLUMN_COURSE_NAME ) );
 		cursor.close();
-		
+
 		//Set the variables that will be used in the AddHomeworkActivity
 		i.putExtra(MainActivity.HW_NAME_TEXT, name);
 		i.putExtra(MainActivity.DATE_TEXT, date);
 		i.putExtra(MainActivity.DESC_TEXT, desc);
 		i.putExtra(MainActivity.COURSE_MNAME, courseName);
-		
+
 		startActivity( i );
 	}
 }
